c5c4341df56c349948249a55968e64a2245bc0d0
Euphoria <hh@Euphoria.cn>
2022-12-26 23:09:37 +0800

    Gateway新一代网关

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GatewayMain9527.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/12/26 22:20
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
