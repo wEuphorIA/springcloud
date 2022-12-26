58d6cbf3e514f0553422d1586c9fdf7e246261cf
Euphoria <hh@Euphoria.cn>
2022-12-27 00:20:16 +0800

    Gateway新一代网关

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/12/26 23:32
+ */
+public class T2 {
+    public static void main(String[] args) {
+        //获取当前时间串
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+        //2020-06-17T11:53:40.325+08:00[Asia/Shanghai]
+    }
+}
