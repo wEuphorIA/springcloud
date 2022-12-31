696f4151fff34ff53d7281b092e38a4fdba8280b
Euphoria <hh@Euphoria.cn>
2022-12-31 20:20:42 +0800

    SpringCloud Sleuth分布式请求链路追踪

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -84,4 +84,12 @@ public class OrderController {
 
         return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
+
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin(){
+        String result = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin", String.class);
+        return result;
+    }
+
+
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -82,4 +82,9 @@ public class PaymentController {
         return serverPort;
     }
 
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin(){
+        return "hi ,i'am paymentzipkin server fall back，welcome to atguigu，O(∩_∩)O哈哈~...";
+    }
+
 }
